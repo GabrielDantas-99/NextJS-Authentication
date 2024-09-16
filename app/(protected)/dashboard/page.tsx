7141f3dc6c41c6a7d6e0d86165190cb4de9f314c
Gabriel <gabrieldantaslp@gmail.com>
2024-09-16 11:47:51 -0300

    feat: Adding role atributte on session callback

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,12 +1,28 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 
 export default async function DashboardPage() {
   const session = await auth();
   return (
-    <div>
-      DashboardPage
+    <main>
+      <header className="bg-blue-200 flex justify-between px-8 py-4">
+        <h2>DashboardPage</h2>
+        <form action={async () => {
+          'use server';
+
+          await signOut();
+        }}>
+          <Button
+            type="submit"
+            variant="destructive"
+            className="px-4 py-2 border border-zinc-400 rounded"
+          >
+            Logout
+          </Button>
+        </form>
+      </header>
       <p>{JSON.stringify(session)}</p>
-    </div>
+    </main>
   );
 }
--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +15,25 @@ export const {
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
 });
