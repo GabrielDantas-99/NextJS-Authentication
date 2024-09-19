be33ab3d8f298410da98035cc89c4071fa0bab83
Gabriel <gabrieldantaslp@gmail.com>
2024-09-19 19:27:20 -0300

    feat: Sending emails (confirmation) for register

--- auth.ts
+++ auth.ts
@@ -16,6 +16,17 @@ export const {
   session: { strategy: "jwt" },
   ...authConfig,
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user?.id);
+
+      // Prevent sign in whithout email verification
+      if (!existingUser?.emailVerified) return false;
+
+      // Add 2FA check
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
